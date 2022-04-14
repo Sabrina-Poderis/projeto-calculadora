808c6562f036fa26a642acd3f11756cfac17b7e5
Sabrina Poderis <sabrina.poderis@gmail.com>
2022-04-14 12:27:22 -0300

    creating calculator component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+import './index.css'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Calculator from './main/Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Calculator />
   </React.StrictMode>
 );
